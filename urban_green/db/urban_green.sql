e47e6d451188cb4bd2942230b1416d3255d4af26
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-10 16:57:29 +0000

    created tables and added sql runner file

---
+++ urban_green/db/urban_green.sql
@@ -0,0 +1,34 @@
+DROP TABLE stock;
+DROP TABLE products;
+DROP TABLE suppliers;
+DROP TABLE categories;
+
+CREATE TABLE categories (
+  id SERIAL2 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE suppliers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  contact_name VARCHAR(255),
+  email VARCHAR(255),
+  phone_number VARCHAR(255)
+);
+
+CREATE TABLE products (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  category_id INT8 REFERENCES categories(id),
+  supplier_id INT4 REFERENCES suppliers(id)
+);
+
+CREATE TABLE stock (
+  id SERIAL8 PRIMARY KEY,
+  product_id INT8 REFERENCES products(id),
+  quantity INT,
+  buying_cost DECIMAL(10,2),
+  selling_price DECIMAL(10,2),
+  reorder_level INT
+);

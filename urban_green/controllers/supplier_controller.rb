8fde20da3a12a1861f1313dade220e38b69b2d80
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-11 13:33:54 +0000

    added for to add new supplier

--- urban_green/controllers/supplier_controller.rb
+++ urban_green/controllers/supplier_controller.rb
@@ -7,3 +7,12 @@ get '/suppliers' do
   @suppliers = Supplier.all()
   erb(:"suppliers/index")
 end
+
+get '/suppliers/new' do
+  erb(:"suppliers/new")
+end
+
+post '/suppliers' do
+  Supplier.new(params).save
+  redirect to '/suppliers'
+end

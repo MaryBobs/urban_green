736163ea81ff320744b4a74f7b23bd1dd6ed9aab
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-11 14:01:06 +0000

    added new product form

--- urban_green/controllers/product_controller.rb
+++ urban_green/controllers/product_controller.rb
@@ -1,10 +1,23 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative("../models/product.rb")
+require_relative("../models/supplier.rb")
+require_relative("../models/category.rb")
 also_reload("../models/*")
 
 get '/products' do
   @products = Product.all()
   @suppliers = Supplier.all()
   erb(:'products/index')
+end
+
+get '/products/new' do
+  @categories = Category.all()
+  @suppliers = Supplier.all()
+  erb(:"products/new")
+end
+
+post '/products' do
+  Product.new(params).save
+  redirect to '/products'
 end

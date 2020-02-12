3c8d12b7971c69af8fe559fd27f3a491ee6585bb
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-02-12 11:39:01 +0000

    update database settings

--- urban_green/db/sql_runner.rb
+++ urban_green/db/sql_runner.rb
@@ -4,9 +4,9 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'df1ujgehc75tdd', 
-      host: 'ec2-184-72-235-80.compute-1.amazonaws.com',
-      port: '5432', user: 'akkhfscqfmcyut', password: 'b07172dc901e15daaa20422f0d8a1453d3c40daf4729458598c227a10d563358'})
+      db = PG.connect({dbname: 'deh47k169dungc', 
+      host: 'ec2-3-231-46-238.compute-1.amazonaws.com',
+      port: '5432', user: 'svrkbunypnzzkm', password: '54f58764d06cfb7bab6bfc7bf4ec9d4c6eb03f4941ddfb5bb8778a91652202a9'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
